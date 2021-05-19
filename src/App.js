9d6d96c39019c8adb2d047645c55414bf86a5a7a
slavo3dev <slavo@slavo3.com>
2021-05-19 14:40:17 -0400

    update data from fake api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
 
+  const [ moviesList, setMoviesList ] = useState([])
+  
+  function fetchMoviesHandler() {
+    fetch("https://swapi.dev/api/films/").then(response => response.json()).then(data => {
+      const trasfromData = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+      setMoviesList(trasfromData);
+    }).catch(err => console.error("Error: ",err))
+  }
+  
+  console.log("Movies: ", moviesList)
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={() => { fetchMoviesHandler()}} >Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={moviesList} />
       </section>
     </React.Fragment>
   );
